f15ed3893b20c2e9d3ee963d12649362d0d538c8
Joe Hillenbrand <jhillenbrand@wetpaint.com>
2012-06-19 13:58:05 -0700

    Added sources.list

---
+++ manifests/sources.pp
@@ -0,0 +1,26 @@
+# source.pp
+# add an apt source
+
+define apt::sources(
+  host = false,
+) {
+
+  if ! $host {
+    $apt_host = $name
+  } else {
+    $apt_host = $host
+  }
+
+  include apt::params
+
+  file { "sources.list":
+    path => "${apt::params::root}/sources.list",
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => 644,
+    content => template("apt/sources.list.erb"),
+    notify => Exec["apt_update"],
+  }
+
+}
